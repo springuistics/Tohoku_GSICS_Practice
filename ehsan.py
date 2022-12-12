ffd22e9216922ccb07a68e5372e64333b26d3d1f
Ehsan796 <120366713+Ehsan796@users.noreply.github.com>
2022-12-12 17:15:09 +0900

    Add files via upload

--- ehsan.py
+++ ehsan.py
@@ -1,1 +1,24 @@
+import tkinter
 
+def count_words(data):
+    string_analysis = data.split(" ")
+    word_count = len(string_analysis)
+    return word_count
+
+def count_sentences(data):
+    periods = data.count(".")
+    exclamation = data.count('!')
+    question = data.count('?')
+    number_of_sentences = periods + exclamation + question
+    return number_of_sentences
+
+def safe_MLS(x, y):
+    if x == 0:
+        return 0
+    if y == 0:
+        return x
+    else:
+        return x/y
+
+def begin():
+    input_data = user_
