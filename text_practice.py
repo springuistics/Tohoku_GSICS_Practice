d5770198f8cf23108ca97f94f729ffe0ca48bed1
springuistics <94886996+springuistics@users.noreply.github.com>
2022-12-12 17:46:19 +0900

    Update text_practice.py

--- text_practice.py
+++ text_practice.py
@@ -22,12 +22,18 @@ def safe_MLS(x, y):
 
 def begin():
     input_data = user_input.get()
-    word_count = count_words(input_data)
-    sentence_count = count_sentences(input_data)
+    please_analyze = get_the_text(input_data)
+    word_count = count_words(please_analyze)
+    sentence_count = count_sentences(please_analyze)
     MLS = safe_MLS(word_count, sentence_count)
     output.config(text=f"Your number of words is: {word_count}, and your MLS is: {MLS}")
 
 
+def get_the_text(filename):
+    the_text = open(filename, "rt")
+        #The four modes are "r" for read, "a" for append, "w" for write, and "x" for create.
+        #The "t" stands for text. If you don't add "r" or "t," python assumes you want to read a text file.
+    return the_text
 
 
 
@@ -37,7 +43,7 @@ window.minsize(800, 400)
 
 user_input = tkinter.StringVar()
 
-my_text = tkinter.Label(window, text="Insert your text below", font=16, fg="blue", background="white")
+my_text = tkinter.Label(window, text="Please copy and paste your file name", font=16, fg="blue", background="white")
 my_text.pack()
 
 username = tkinter.Entry(window, textvariable=user_input, width=60)
