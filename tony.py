67281f4e6bf59aabd833d04d0b96ecd3c728f2b6
YutoVega <tonysan0322@hotmail.com>
2022-12-19 18:22:08 +0900

    I also updated tony.py

--- tony.py
+++ tony.py
@@ -26,7 +26,7 @@ def begin():
     sentence_count = count_sentences(input_data)
     MLS = safe_MLS(word_count, sentence_count)
     if word_count < 10:
-        return output.config(text="Your number of words is not enough")
+        return output.config(text="Your number of birds is not enough")
     elif MLS < 6:
         return output.config(text="Please use longer sentences")
     else:
